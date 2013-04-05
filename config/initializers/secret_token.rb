440e919d283a249eb1b03f7d33af4a0457b697fb
Hernan Dario Metaute Sarmiento <hernan.metaute@ceiba.com.co>
2013-04-05 11:36:59 -0500

    El primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inventario::Application.config.secret_token = '00bf73371c42e50cad00991bbfb0fda283c31e70b097e13311392e304d5a3f60ab4f4bb777d7475d8b2defa84c1441e8aa6df5d25b805167381d11921dbc75c8'
